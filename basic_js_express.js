e60927e7fcdffeb9b91d2bf7262a32d5e225a3af
tengfone <phangtf@gmail.com>
2020-01-05 23:29:45 +0800

    Completed tutorial

--- basic_js_express.js
+++ basic_js_express.js
@@ -1,11 +1,14 @@
 let express = require('express');
 let app = express();
+var bodyParser = require('body-parser')
 
 // ejs set view engine
 app.set('view engine','ejs');
+// applying css static files
+app.use('/assets',express.static('assets'));
 
 app.get('/',function(req,res){
-    res.sendFile(__dirname + '/index.html');
+    res.render('index');
 });
 
 app.get('/content',function(req,res){
@@ -19,8 +22,20 @@ app.get('/content',function(req,res){
 
 // using EJS
 app.get('/profile/:name',function(req,res){
+    // let data is considered middle ware codes
     let data = {age:20,job:'God',hobby:['eating','sleeping','playing']};
     res.render('profile',{person:req.params.name, data:data});
+});
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+// getting query data and displaying
+app.get('/contact',function(req,res){
+    res.render('contact',{qs:req.query});
+});
+
+// parser
+app.post('/contact',urlencodedParser,function(req,res){
+    res.render('contact-success',{data:req.body});
 });
 
 app.listen(3000);
